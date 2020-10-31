e5970030f86b085c0e15ad1192b20874865c5acc
PredaMihaiDragos <48451420+PredaMihaiDragos@users.noreply.github.com>
2020-10-31 19:55:49 +0200

    Choose between: acteBanca, acteRegister and acte at acteView

--- itoit/Views/acteView.py
+++ itoit/Views/acteView.py
@@ -6,4 +6,8 @@ from itoit import app
 
 @app.route("/acte", methods=['GET'])
 def acte():
+    if current_user.bank_id is not None:
+        return render_template("actebanca.jinja", user=current_user)
+    else if current_user.trade_register_id is not None:
+        return render_template("acteregister.jinja", user=current_user)
     return render_template("acte.jinja", user=current_user)
