4728230781d986b66f75030bc466b6180874e89d
bazycristi21 <bazycristi21@yahoo.com>
2020-10-31 19:59:08 +0200

    approve/denied fix

--- itoit/static/js/acte.mjs
+++ itoit/static/js/acte.mjs
@@ -3,6 +3,12 @@ import {getFirmsProgress} from "./API/API.mjs";
 // bank_documents_approved
 // register_documents_approved
 // factura_capital_approved
+function disableAnchor(anchor)
+{
+    anchor.removeAttribute('href');
+    anchor.classList.add('disabled');
+    anchor.setAttribute('color','grey');
+}
 function updateProgress() // functie care updateaza progresul de creare a firmei
 {
     let approved = document.querySelectorAll(".fa-check");
@@ -10,18 +16,17 @@ function updateProgress() // functie care updateaza progresul de creare a firmei
     let steps = document.querySelectorAll(".step-a");
     getFirmsProgress().then((data) => {
         console.log(data);
-        steps[1].setAttribute('disabled',true);
-        steps[2].setAttribute('disabled',true);
-            console.log("aaaa");
+       
         if(data[0]['bank_documents_approved']  === false)
         {
             approved[0].style.display = 'none';
             approved[1].style.display = 'none';
             approved[2].style.display = 'none';
             denied[0].style.display = 'block';
-            denied[0].style.display = 'block';
-            denied[0].style.display = 'block';
-            
+            denied[1].style.display = 'block';
+            denied[2].style.display = 'block';
+            disableAnchor(steps[1]);
+            disableAnchor(steps[2]);
             
         }
         else if(data[0]['factura_capital_approved'] === false)
@@ -30,8 +35,9 @@ function updateProgress() // functie care updateaza progresul de creare a firmei
             approved[1].style.display = 'none';
             approved[2].style.display = 'none';
             denied[0].style.display = 'none';
-            denied[0].style.display = 'block';
-            denied[0].style.display = 'block';
+            denied[1].style.display = 'block';
+            denied[2].style.display = 'block';
+            disableAnchor(steps[2]);
         }
         else if(data[0]['register_documents_approved'] === false)
         {
@@ -39,8 +45,8 @@ function updateProgress() // functie care updateaza progresul de creare a firmei
             approved[1].style.display = 'block';
             approved[2].style.display = 'none';
             denied[0].style.display = 'none';
-            denied[0].style.display = 'none';
-            denied[0].style.display = 'block';
+            denied[1].style.display = 'none';
+            denied[2].style.display = 'block';
         }
         else
         {
@@ -48,8 +54,8 @@ function updateProgress() // functie care updateaza progresul de creare a firmei
             approved[1].style.display = 'block';
             approved[2].style.display = 'block';
             denied[0].style.display = 'none';
-            denied[0].style.display = 'none';
-            denied[0].style.display = 'none';
+            denied[1].style.display = 'none';
+            denied[2].style.display = 'none';
         }
     })
 
