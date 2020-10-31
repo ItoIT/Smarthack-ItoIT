b0ff3fba01a748b016be930b873065e0d60b8f39
bazycristi21 <bazycristi21@yahoo.com>
2020-10-31 21:21:38 +0200

    register remember

--- itoit/static/js/acte.mjs
+++ itoit/static/js/acte.mjs
@@ -16,49 +16,59 @@ function updateProgress() // functie care updateaza progresul de creare a firmei
     let steps = document.querySelectorAll(".step-a");
     getFirmsProgress().then((data) => {
         console.log(data);
-       
-        if(data[0]['bank_documents_approved']  === false)
+        if(data.length > 0 )
         {
-            approved[0].style.display = 'none';
-            approved[1].style.display = 'none';
-            approved[2].style.display = 'none';
-            denied[0].style.display = 'block';
-            denied[1].style.display = 'block';
-            denied[2].style.display = 'block';
-            disableAnchor(steps[1]);
-            disableAnchor(steps[2]);
-            
-        }
-        else if(data[0]['factura_capital_approved'] === false)
-        {
-            approved[0].style.display = 'block';
-            approved[1].style.display = 'none';
-            approved[2].style.display = 'none';
-            denied[0].style.display = 'none';
-            denied[1].style.display = 'block';
-            denied[2].style.display = 'block';
-            disableAnchor(steps[2]);
-        }
-        else if(data[0]['register_documents_approved'] === false)
-        {
-            approved[0].style.display = 'block';
-            approved[1].style.display = 'block';
-            approved[2].style.display = 'none';
-            denied[0].style.display = 'none';
-            denied[1].style.display = 'none';
-            denied[2].style.display = 'block';
+            if(data[0]['bank_documents_approved']  === false)
+            {
+                approved[0].style.display = 'none';
+                approved[1].style.display = 'none';
+                approved[2].style.display = 'none';
+                denied[0].style.display = 'block';
+                denied[1].style.display = 'block';
+                denied[2].style.display = 'block';
+                disableAnchor(steps[1]);
+                disableAnchor(steps[2]);
+                
+            }
+            else if(data[0]['factura_capital_approved'] === false)
+            {
+                approved[0].style.display = 'block';
+                approved[1].style.display = 'none';
+                approved[2].style.display = 'none';
+                denied[0].style.display = 'none';
+                denied[1].style.display = 'block';
+                denied[2].style.display = 'block';
+                disableAnchor(steps[2]);
+            }
+            else if(data[0]['register_documents_approved'] === false)
+            {
+                approved[0].style.display = 'block';
+                approved[1].style.display = 'block';
+                approved[2].style.display = 'none';
+                denied[0].style.display = 'none';
+                denied[1].style.display = 'none';
+                denied[2].style.display = 'block';
+            }
+            else
+            {
+                approved[0].style.display = 'block';
+                approved[1].style.display = 'block';
+                approved[2].style.display = 'block';
+                denied[0].style.display = 'none';
+                denied[1].style.display = 'none';
+                denied[2].style.display = 'none';
+                let completed = document.querySelector("#firm-completed");
+                completed.style.display = 'block';
+                completed.setAttribute('href',data['acte_complete_url']);
+            }
         }
         else
         {
-            approved[0].style.display = 'block';
-            approved[1].style.display = 'block';
-            approved[2].style.display = 'block';
-            denied[0].style.display = 'none';
-            denied[1].style.display = 'none';
-            denied[2].style.display = 'none';
-            let completed = document.querySelector("#firm-completed");
-            completed.style.display = 'block';
-            completed.setAttribute('href',data['acte_complete_url']);
+                denied[0].style.display = 'block';
+                denied[1].style.display = 'block';
+                denied[2].style.display = 'block';
+                disableAnchor(steps[1]);
+                disableAnchor(steps[2]);
         }
     })
 
